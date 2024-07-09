7e8c5959f45ea3581d2ad61f0c07bbddc4b9a7c2
MatthewKelseyRT <matthew.kelsey@rising-tide.ai>
2024-07-09 09:08:09 +0100

    commented example expanding approx

--- code/basic/test_approx.py
+++ code/basic/test_approx.py
@@ -5,3 +5,6 @@ from rpncalc.utils import calc
 def test_add():
     res = calc(0.2, 0.1, "+")
     assert res == pytest.approx(0.3)
+
+# Can override tolerance (rel/abs), e.g. 20 ± 2°:
+# assert temperature == pytest.approx(20, abs=2)
