46f12d5e268823eb42f62e9c4dd3afd5731a4f29
MatthewKelseyRT <matthew.kelsey@rising-tide.ai>
2024-07-09 09:06:29 +0100

    Added some raise ValueError tests

--- code/basic/test_raises.py
+++ code/basic/test_raises.py
@@ -6,3 +6,16 @@ import pytest
 def test_zero_division():
     with pytest.raises(ZeroDivisionError):
         calc(3, 0, "/")
+
+def test_invalid_operator():
+    with pytest.raises(ValueError):
+        calc(3, 0, "%")
+
+def test_invalid_operator_regex_match():
+    with pytest.raises(ValueError, match=r"Invalid operator"):
+        calc(3, 0, "%")
+
+def test_invalid_operator_as():
+    with pytest.raises(ValueError) as excinfo:
+        calc(3, 0, "%")
+    assert str(excinfo.value) == "Invalid operator: %"
