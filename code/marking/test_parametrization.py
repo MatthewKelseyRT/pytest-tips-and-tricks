b1fe8f6cade58aa3d2b0cfcd2f99ecb79b8e8480
MatthewKelseyRT <matthew.kelsey@rising-tide.ai>
2024-07-09 09:30:32 +0100

    param test ids

--- code/marking/test_parametrization.py
+++ code/marking/test_parametrization.py
@@ -23,3 +23,22 @@ def test_smoke(op):
 @pytest.mark.parametrize("b", [3, 4])
 def test_permutations(a, b):
     assert calc(a, b, "+") == a + b
+
+
+# Giving ids to the test cases
+@pytest.mark.parametrize(
+    "a, b, op, expected", [
+    (1, 1, "+", 2),
+    (3, 1, "-", 2),
+], ids=["1+1", "3-1"]) # Can also pass a callable to generate the id, so that we can have dynamic ids
+def test_ids(a, b, expected):
+    assert calc(a, b, "+") == expected
+
+# Can also give id at the point of defining the test case
+@pytest.mark.parametrize(
+    "a, b, op, expected", [
+    pytest.param(1, 2, "+", 3, id="1+2"),
+    pytest.param(3, 1, "-", 5, id="3-1"), # This is better than the default test id "3-1---5"
+])
+def test_id_per_test(a, b, op, expected):
+    assert calc(a, b, op) == expected
